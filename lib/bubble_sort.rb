948c7ac983b53216debde71924221777ca06d481
Julian Feliciano <julsfelic@gmail.com>
2015-12-11 08:55:03 -0700

    sort can take a two element array and sort

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,7 +1,30 @@
 require 'pry'
 
 class BubbleSort
+
   def sort(unsorted_array=[])
+    return unsorted_array if unsorted_array.count < 2
+    swap_count = -1
+    current_element = unsorted_array[0]
+    next_element    = unsorted_array[1]
+    unsorted_array_count = unsorted_array.count
+    count = 1
+    until swap_count == 0
+      swap_count = 0
+      while count < unsorted_array_count
+        if current_element > next_element
+          current_element_index = unsorted_array.index(current_element)
+          next_element_index = unsorted_array.index(next_element)
+          unsorted_array[current_element_index]     = next_element
+          unsorted_array[next_element_index]        = current_element
+          current_element = next_element
+          next_element = unsorted_array[next_element_index + 1]
+          swap_count += 1
+          count += 1
+        end
+      end
+    end
     unsorted_array
   end
+
 end
