927fd9f2ede0c1b2a7de89db0563760b69a22bc8
Julian Feliciano <julsfelic@gmail.com>
2015-12-11 09:23:04 -0700

    sort can sort a three element array

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -9,19 +9,17 @@ class BubbleSort
     next_element    = unsorted_array[1]
     unsorted_array_count = unsorted_array.count
     count = 1
-    until swap_count == 0
+    # binding.pry
+    until swap_count == 0 || next_element.nil?
       swap_count = 0
-      while count < unsorted_array_count
-        if current_element > next_element
-          current_element_index = unsorted_array.index(current_element)
-          next_element_index = unsorted_array.index(next_element)
-          unsorted_array[current_element_index]     = next_element
-          unsorted_array[next_element_index]        = current_element
-          current_element = next_element
-          next_element = unsorted_array[next_element_index + 1]
-          swap_count += 1
-          count += 1
-        end
+      if current_element > next_element
+        current_element_index = unsorted_array.index(current_element)
+        next_element_index = unsorted_array.index(next_element)
+        unsorted_array[current_element_index]     = next_element
+        unsorted_array[next_element_index]        = current_element
+        current_element = next_element
+        next_element = unsorted_array[next_element_index + 1]
+        swap_count += 1
       end
     end
     unsorted_array
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -25,4 +25,9 @@ class BubbleSortTest < Minitest::Test
   def test_sort_returns_sorted_array_of_two_elements
     assert_equal [1, 2], @sorter.sort([2, 1])
   end
+
+  meta t: true
+  def test_sort_returns_sorted_array_of_three_elements
+    assert_equal [1, 2, 3], @sorter.sort([2, 1, 3])
+  end
 end
