83e456cd0e28f28f79f3bb248579e435109d01c9
Julian Feliciano <julsfelic@gmail.com>
2015-12-12 12:16:31 -0700

    Rewrite bubble sort using upto method

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,28 +1,27 @@
 require 'pry'
 
 class BubbleSort
-
-  def sort(unsorted_array=[])
-    return unsorted_array if unsorted_array.count < 2
-    swap_count = -1
-    current_element = unsorted_array[0]
-    next_element    = unsorted_array[1]
-    unsorted_array_count = unsorted_array.count
-    count = 1
-    # binding.pry
-    until swap_count == 0 || next_element.nil?
-      swap_count = 0
-      if current_element > next_element
-        current_element_index = unsorted_array.index(current_element)
-        next_element_index = unsorted_array.index(next_element)
-        unsorted_array[current_element_index]     = next_element
-        unsorted_array[next_element_index]        = current_element
-        current_element = next_element
-        next_element = unsorted_array[next_element_index + 1]
-        swap_count += 1
+  def sort(values=[])
+    return "Give me an Array fool!" unless values.is_a?(Array)
+    swap_value = -1
+    how_many_swap_checks = values.count - 2
+    until swap_value == 0
+      swap_value = 0
+      0.upto(how_many_swap_checks) do |n|
+        previous = values[n]
+        current = values[n + 1]
+        if previous > current
+          values[n] = current
+          values[n + 1] = previous
+          swap_value += 1
+        end
       end
     end
-    unsorted_array
+    values
   end
+end
 
+if __FILE__ == $0
+  sorter = BubbleSort.new
+  sorter.sort([4, 1, 3, 2])
 end
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -10,7 +10,11 @@ class BubbleSortTest < Minitest::Test
     assert_instance_of BubbleSort, @sorter
   end
 
-  def test_sort_returns_an_empty_array_if_nothing_is_passed_in
+  def test_sort_returns_message_if_not_given_an_array
+    assert_equal "Give me an Array fool!", @sorter.sort(8)
+  end
+
+  def test_sort_returns_empty_arary_if_given_no_argument
     assert_equal [], @sorter.sort
   end
 
@@ -26,8 +30,19 @@ class BubbleSortTest < Minitest::Test
     assert_equal [1, 2], @sorter.sort([2, 1])
   end
 
-  meta t: true
   def test_sort_returns_sorted_array_of_three_elements
     assert_equal [1, 2, 3], @sorter.sort([2, 1, 3])
+  end
+
+  def test_sort_returns_sorted_array_of_four_elements
+    assert_equal [1, 2, 3, 4], @sorter.sort([4, 1, 3, 2])
+  end
+
+  def test_sort_returns_sorted_array_of_many_elements
+    assert_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], @sorter.sort([2, 4, 1, 3, 9, 7, 8, 0, 5, 6])
+  end
+
+  def test_sort_returns_sorted_array_when_given_reversed_array
+    assert_equal [1, 2, 3, 4], @sorter.sort([4, 3, 2, 1])
   end
 end
