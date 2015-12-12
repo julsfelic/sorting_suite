595d29471930d6fbe38cee80d0e0545ec03ac633
Julian Feliciano <julsfelic@gmail.com>
2015-12-12 14:22:17 -0700

    Test sort is able to sort three elements

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -12,24 +12,26 @@ class InsertionSort
     if values[1] == nil
       return values
     end
-    # pull out the first value from the unsorted array
-    first_value = values.shift
-    # place that value into the sorted array
-    sorted << first_value
-    # pull out the second value from the unsorted array
-    second_value = values.shift
-    # compare that value to the first value in the sorted array
-    if second_value < sorted[0]
-      sorted.insert(0, second_value)
-    else
-      sorted << second_value
+    amount_of_values = values.count - 2
+
+    0.upto(amount_of_values) do
+      inserted = false
+      sorted << values.shift if sorted.empty?
+      current_sorted_count = sorted.count - 1
+      value = values.shift
+      0.upto(current_sorted_count) do |n|
+        if value < sorted[n]
+          sorted.insert(n, value)
+          inserted = true
+        end
+      end
+      sorted << value unless inserted
     end
-    # if unsorted array is empty, return sorted array
     sorted
   end
 end
 
 if __FILE__ == $0
   sorter = InsertionSort.new
-  sorter.sort([2, 1])
+  sorter.sort([2, 1, 3])
 end
--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -25,8 +25,12 @@ class InsertionSortTest < Minitest::Test
   def test_sort_returns_sorted_array_of_one_element
     assert_equal [1], @sorter.sort([1])
   end
-  
+
   def test_sort_returns_sorted_array_of_two_elements
     assert_equal [1, 2], @sorter.sort([2, 1])
+  end
+  
+  def test_sort_returns_sorted_array_of_three_elements
+    assert_equal [1, 2, 3], @sorter.sort([2, 1, 3])
   end
 end
