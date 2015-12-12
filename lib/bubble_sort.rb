3d124c7df09d64dee2957fcb9e579677cf3d8578
Julian Feliciano <julsfelic@gmail.com>
2015-12-12 12:33:27 -0700

    Refactor out a sort values method from the main sort method

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,27 +1,36 @@
 require 'pry'
 
 class BubbleSort
+  attr_reader :swap_value
+
+  def initialize
+    @swap_value = -1
+  end
+
   def sort(values=[])
     return "Give me an Array fool!" unless values.is_a?(Array)
-    swap_value = -1
-    how_many_swap_checks = values.count - 2
+    amount_of_swap_checks = values.count - 2
     until swap_value == 0
-      swap_value = 0
-      0.upto(how_many_swap_checks) do |n|
-        previous = values[n]
-        current = values[n + 1]
-        if previous > current
-          values[n] = current
-          values[n + 1] = previous
-          swap_value += 1
-        end
-      end
+      sort_values(values, amount_of_swap_checks)
     end
     values
   end
 end
 
+def sort_values(values, amount_of_swap_checks)
+  @swap_value = 0
+  0.upto(amount_of_swap_checks) do |n|
+    previous = values[n]
+    current = values[n + 1]
+    if previous > current
+      values[n] = current
+      values[n + 1] = previous
+      @swap_value += 1
+    end
+  end
+end
+
 if __FILE__ == $0
   sorter = BubbleSort.new
-  sorter.sort([4, 1, 3, 2])
+  p sorter.sort([4, 1, 3, 2])
 end
