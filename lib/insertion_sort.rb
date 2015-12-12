96488c3209efbe97bba6396a098f43745f78cd47
Julian Feliciano <julsfelic@gmail.com>
2015-12-12 13:23:53 -0700

    Test sort returns message if given wrong type of argument and handles no argument at all

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,4 +1,8 @@
 require 'pry'
 
 class InsertionSort
+  def sort(values=[])
+    return "Give me an Array fool!" unless values.is_a?(Array)
+    values
+  end
 end
--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -2,9 +2,23 @@ require 'minitest'
 require 'insertion_sort'
 
 class InsertionSortTest < Minitest::Test
-  def test_can_create_instances
-    sorter = InsertionSort.new
+  def setup
+    @sorter = InsertionSort.new
+  end
 
-    assert_instance_of InsertionSort, sorter
+  def test_can_create_instances
+    assert_instance_of InsertionSort, @sorter
+  end
+
+  def test_sort_returns_message_if_not_given_an_array
+    assert_equal "Give me an Array fool!", @sorter.sort(8)
+  end
+
+  def test_sort_returns_empty_arary_if_given_no_argument
+    assert_equal [], @sorter.sort
+  end
+
+  def test_sort_returns_an_empty_array_if_empty_array_is_passed_in
+    assert_equal [], @sorter.sort([])
   end
 end
