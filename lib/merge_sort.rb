39ba6d0f4acf808a2712b748142d1e1ee92b44a4
Julian Feliciano <julsfelic@gmail.com>
2015-12-13 12:48:28 -0700

    Test sort returns empty array if given no argument

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -4,5 +4,6 @@ class MergeSort
 
   def sort(values=[])
     return "Give me an Array fool!" unless values.is_a?(Array)
+    values
   end
 end
--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -13,4 +13,8 @@ class MergeSortTest < Minitest::Test
   def test_sort_returns_message_if_not_given_an_array
     assert_equal "Give me an Array fool!", @sorter.sort(8)
   end
+
+  def test_sort_returns_empty_arary_if_given_no_argument
+    assert_equal [], @sorter.sort
+  end
 end
