150ef18acd1fa5fc786257495a5868ea939e4558
Julian Feliciano <julsfelic@gmail.com>
2015-12-12 17:39:51 -0700

    Unit test insert value method

--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -29,7 +29,7 @@ class InsertionSortTest < Minitest::Test
   def test_sort_returns_sorted_array_of_two_elements
     assert_equal [1, 2], @sorter.sort([2, 1])
   end
-  
+
   def test_sort_returns_sorted_array_of_three_elements
     assert_equal [1, 2, 3], @sorter.sort([2, 1, 3])
   end
@@ -40,5 +40,12 @@ class InsertionSortTest < Minitest::Test
 
   def test_sort_returns_sorted_array_of_many_elements
     assert_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], @sorter.sort([2, 4, 1, 3, 9, 7, 8, 0, 5, 6])
+  end
+
+  def test_insert_value_properly_inserts_a_value
+    @sorter.sort([4, 2, 1])
+
+    @sorter.insert_value(3)
+    assert_equal [1, 2, 3, 4], @sorter.sorted
   end
 end
