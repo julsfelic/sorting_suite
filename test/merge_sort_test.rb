af29252a734493b6e964cc59cef22f465e052659
Julian Feliciano <julsfelic@gmail.com>
2015-12-13 18:36:07 -0700

    Test sort values returns values in order

--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -1,5 +1,6 @@
 require 'minitest'
 require 'merge_sort'
+require 'pry'
 
 class MergeSortTest < Minitest::Test
   def setup
@@ -37,12 +38,17 @@ class MergeSortTest < Minitest::Test
   def test_sort_returns_sorted_array_of_four_elements
     assert_equal [1, 2, 3, 4], @sorter.sort([4, 1, 3, 2])
   end
-  
+
   def test_sort_returns_sorted_array_of_many_elements
     assert_equal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], @sorter.sort([2, 4, 1, 3, 9, 7, 8, 0, 5, 6])
   end
 
   def test_can_sort_letters
     assert_equal ['a', 'c', 'j', 'm','z'], @sorter.sort(['a', 'z', 'c', 'm', 'j'])
+  end
+
+  def test_sort_values_returns_values_in_order
+    sorted_values = @sorter.sort_values([3, 2, 4, 1])
+    assert_equal [1, 2, 3, 4], sorted_values
   end
 end
